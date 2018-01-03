2ab207bc21b0c97277bb8f6ab41e8de35a6003bd
harishkannarao <harish.gtec@gmail.com>
2018-01-03 18:55:03 +0000

    Creating separate module for JavaScript jasmine unit tests

---
+++ JavaScriptJasmineUnitTests/src/main/java/com/harishkannarao/demo/jasmine_tests/JasmineUnitTestsApplication.java
@@ -0,0 +1,11 @@
+package com.harishkannarao.demo.jasmine_tests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JasmineUnitTestsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JasmineUnitTestsApplication.class, args);
+    }
+}
